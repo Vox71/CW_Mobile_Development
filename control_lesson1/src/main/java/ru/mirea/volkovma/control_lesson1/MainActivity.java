48b79f85f97239e1d92d2f9cd40e33d50ed79596
Vox71 <mkellevolkov@gmail.com>
2023-02-18 19:19:52 +0300

    control_lesson1 1st commit

--- control_lesson1/src/main/java/ru/mirea/volkovma/control_lesson1/MainActivity.java
+++ control_lesson1/src/main/java/ru/mirea/volkovma/control_lesson1/MainActivity.java
@@ -1,21 +1,14 @@
-package ru.mirea.volkovma.typelayout;
+package ru.mirea.volkovma.control_lesson1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tvMirea;
-    private Button btnChangeMireaText
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvMirea = findViewById(R.id.textViewMirea);
-        btnChangeMireaText = findViewById(R.id.buttonChangeMireaText);
-
     }
 }
