d794a774f8c41d971c2b1f112fe907c9110f646d
Vox71 <mkellevolkov@gmail.com>
2023-03-02 16:13:00 +0300

    0.1 added layouts, before count test

--- control_lesson1/src/main/java/ru/mirea/volkovma/control_lesson1/MainActivity.java
+++ control_lesson1/src/main/java/ru/mirea/volkovma/control_lesson1/MainActivity.java
@@ -1,14 +1,37 @@
 package ru.mirea.volkovma.control_lesson1;
 
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int mCounter = 0;
+    Button btn;
+    TextView txv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.button_class);
+        txv = (TextView) findViewById(R.id.editTextNumber);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCounter ++;
+                txv.setText (Integer.toString(mCounter));
+            }
+        });
+    }
+
+    public void onButtonClick(View view) {
+
+
     }
 }
